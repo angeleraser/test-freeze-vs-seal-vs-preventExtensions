140a358d73657b517da54a3f07fe416f5ad437b5
Angel Figuera <angeleraser@gmail.com>
2022-09-15 18:40:38 -0400

    add new test cases for better testing

--- test/unit/specs/test.functions.js
+++ test/unit/specs/test.functions.js
@@ -1,99 +1,127 @@
-'use strict';
+"use strict";
 
-['freeze', 'seal', 'preventExtensions'].forEach((name) => {
-    let method = Object[name];
+const methodNames = ["freeze", "seal", "preventExtensions"];
 
-    describe(`${name} (strict mode: enabled)`, () => {
-        describe('[C] add new property', () => {
-            it('primary object', () => {
-                let emptyObject = {};
-                method(emptyObject);
+methodNames.forEach((name) => {
+  const method = Object[name];
 
-                expect(() => {
-                    emptyObject.test = 1;
-                }).not.toThrow();
-                expect(emptyObject.test).toEqual(1);
-            });
+  describe(`Testing Object.${name}()`, () => {
+    describe("[Create] new property", () => {
+      it("primitive", () => {
+        const emptyObject = {};
 
-            it('returned object', () => {
-                let emptyObject = {};
-                let emptyWrappedObject = method(emptyObject);
+        method(emptyObject);
 
-                expect(() => {
-                    emptyWrappedObject.test = 1;
-                }).not.toThrow();
-                expect(emptyWrappedObject.test).toEqual(1);
-            });
-        });
+        expect(() => {
+          emptyObject.test = "create";
+        }).not.toThrow();
 
-        describe('[R] read existing property', () => {
-            it('primary object', () => {
-                let emptyObject = {
-                    test: 2
-                };
-                method(emptyObject);
-                expect(emptyObject.test).toEqual(2);
-            });
+        expect(() => {
+          Object.defineProperty(emptyObject, "test", {
+            writable: true,
+            enumerable: true,
+            value: "create",
+          });
+        }).not.toThrow();
 
-            it('returned object', () => {
-                let emptyObject = {
-                    test: 2
-                };
-                let emptyWrappedObject = method(emptyObject);
-                expect(emptyWrappedObject.test).toEqual(2);
-            });
-        });
-
-        describe('[U] modify existed property', () => {
-            it('primary object', () => {
-                let emptyObject = {
-                    test: 2
-                };
-                method(emptyObject);
-
-                expect(() => {
-                    emptyObject.test = 3;
-                }).not.toThrow();
-                expect(emptyObject.test).toEqual(3);
-            });
-
-            it('returned object', () => {
-                let emptyObject = {
-                    test: 2
-                };
-                let emptyWrappedObject = method(emptyObject);
-
-                expect(() => {
-                    emptyWrappedObject.test = 3;
-                }).not.toThrow();
-                expect(emptyWrappedObject.test).toEqual(3);
-            });
-        });
-
-        describe('[D] remove existed property', () => {
-            it('primary object', () => {
-                let emptyObject = {
-                    test: 2
-                };
-                method(emptyObject);
-
-                expect(() => {
-                    delete emptyObject.test;
-                }).not.toThrow();
-                expect(emptyObject.test).toEqual(undefined);
-            });
-
-            it('returned object', () => {
-                let emptyObject = {
-                    test: 2
-                };
-                let emptyWrappedObject = method(emptyObject);
-
-                expect(() => {
-                    delete emptyWrappedObject.test;
-                }).not.toThrow();
-                expect(emptyWrappedObject.test).toEqual(undefined);
-            });
-        });
+        expect(emptyObject.test).toEqual("create");
+      });
     });
+
+    describe("[Read] existing property", () => {
+      it("primitive", () => {
+        const emptyObject = {
+          test: "read",
+        };
+
+        method(emptyObject);
+
+        expect(emptyObject.test).toEqual("read");
+      });
+    });
+
+    describe("[Update] existing property", () => {
+      it("primitive", () => {
+        const emptyObject = {
+          test: "",
+        };
+
+        method(emptyObject);
+
+        expect(() => {
+          Object.defineProperty(emptyObject, "test", {
+            writable: true,
+            enumerable: true,
+            value: "update",
+          });
+        }).not.toThrow();
+
+        expect(emptyObject.test).toEqual("update");
+      });
+
+      it("non primitive", () => {
+        const emptyObject = {
+          one: { name: "test" },
+          two: { name: "test" },
+        };
+
+        method(emptyObject);
+
+        expect(() => {
+          emptyObject.one.name = "new defined value";
+        }).not.toThrow();
+
+        expect(() => {
+          Object.defineProperty(emptyObject.two, "name", {
+            writable: true,
+            enumerable: true,
+            value: "new defined value",
+          });
+        }).not.toThrow();
+
+        expect(emptyObject.one.name).toEqual("new defined value");
+        expect(emptyObject.two.name).toEqual("new defined value");
+      });
+    });
+
+    describe("[Delete] existing property", () => {
+      it("primitive", () => {
+        const emptyObject = {
+          test: "delete",
+        };
+
+        method(emptyObject);
+
+        expect(() => {
+          delete emptyObject.test;
+        }).not.toThrow();
+
+        expect(emptyObject.test).toEqual(undefined);
+      });
+
+      it("non primitive", () => {
+        const emptyObject = {
+          one: { name: "test" },
+          two: { name: "test" },
+        };
+
+        method(emptyObject);
+
+        expect(() => {
+          delete emptyObject.one;
+        }).not.toThrow();
+
+        expect(() => {
+          Object.defineProperty(emptyObject, "two", {
+            writable: true,
+            enumerable: true,
+            value: undefined,
+          });
+        }).not.toThrow();
+
+        expect(emptyObject.one).toEqual(undefined);
+        expect(emptyObject.two).toEqual(undefined);
+      });
+    });
+  });
 });
